7ce5c58fe324b98a429a00cf311d4ba086da8268
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-04 17:41:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < rankedList.length; i++) {
-    rankedList[i].innerHTML = (i + n).toString();
+    rankedList[i].innerHTML = parseInt(i + n).toString();
   }
 }
 

5a49f309ac542954dd2202a53f6a7e62da708ba1
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-04 17:15:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function increaseRankBy(n) {
   for (let i = 0; i < rankedList.length; i++) {
     rankedList[i].innerHTML = (i + n).toString();
   }
-  return rankedList[i];
+  return rankedList;
 }
 
 function deepestChild() {
